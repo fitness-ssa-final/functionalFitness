aa7031e5fb15761e10be0a6152e2cdec9287d794
Jamal McDowell <jamal.l.mcdowell@gmail.com>
2016-10-06 11:56:42 -0400

    refactoring

--- fITnessapp/src/main/webapp/JS/fitnessapp.js
+++ fITnessapp/src/main/webapp/JS/fitnessapp.js
@@ -1,39 +1,31 @@
-$.get('https://wger.de/api/v2/exercise.json/?language=2&limit=999&equipment=1',
-		function(data) {
-			for (var i = 0; i < data.results.length; i++) {
-				data[i];
+function connectToAPI() {
+    $.get("https://wger.de/api/v2/exercise.json/?language=2&limit=999&equipment=1",
+        parseAPIResponse(data));
+}
 
-				var name = data.results[i].name;
-				console.log('Name : ', name);
+function parseAPIResponse(data) {
+    for (var i = 0; i < data.results.length; i++) {
+        // data[i];
 
-				var description = data.results[i].description;
-				console.log('Description : ', description);
+        var name = data.results[i].name;
+        var description = data.results[i].description;
+        var muscleCategory = data.results[i].category;
+        var equipment = data.results[i].equipment;
+        var index = equipment.indexOf(1);
 
-				var muscleCategory = data.results[i].category;
-				console.log('Muscle Category : ', muscleCategory);
+        if (index !== -1) {
+            equipment[index] = "Barbell";
 
-				var equipment = data.results[i].equipment;
-				console.log('Equipment : ', equipment);
-				
-//				if (muscleCategory.equals) {
-//					
-//				} else {
-//
-//				}
+        }
 
-				var index = equipment.indexOf(1);
-				if (index !== -1) {
-					equipment[index] = "Barbell"
-
-				}
-
-				$('body').append(
-						"Name:   " + name + '<hr>' + "Directions:   "
-								+ description + '<hr>' + "Muscle Category:   "
-								+ muscleCategory + '<hr>'
-								+ "Equipment Needed:    " + equipment
-								+ '<hr><br><br>');
-
-			}
-
-		});
+        $('body').append(
+            "Name:   " + name + '<hr>' + "Directions:   " +
+            description + '<hr>' + "Muscle Category:   " +
+            muscleCategory + '<hr>' + "Equipment Needed:    " +
+            equipment + '<hr><br><br>');
+    }
+    //				if (muscleCategory.equals) {
+    //
+    //				} else {
+    //
+}
