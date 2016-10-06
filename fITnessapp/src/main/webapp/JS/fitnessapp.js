76fa36bd3060e98f103bc98de246149bdf69ab19
Jamal McDowell <jamal.l.mcdowell@gmail.com>
2016-10-06 16:58:15 -0400

    Got data to show on table

--- fITnessapp/src/main/webapp/JS/fitnessapp.js
+++ fITnessapp/src/main/webapp/JS/fitnessapp.js
@@ -1,31 +1,102 @@
-function connectToAPI() {
-    $.get("https://wger.de/api/v2/exercise.json/?language=2&limit=999&equipment=1",
-        parseAPIResponse(data));
-}
+$(function() {
 
-function parseAPIResponse(data) {
-    for (var i = 0; i < data.results.length; i++) {
-        // data[i];
 
-        var name = data.results[i].name;
-        var description = data.results[i].description;
-        var muscleCategory = data.results[i].category;
-        var equipment = data.results[i].equipment;
-        var index = equipment.indexOf(1);
+    $.get('https://wger.de/api/v2/exercise.json/?language=2&limit=1000000&equipment=1',
+        function(data) {
 
-        if (index !== -1) {
-            equipment[index] = "Barbell";
+            var muscleCategories = [{
+                "id": 10,
+                "name": "Abs"
+            }, {
+                "id": 8,
+                "name": "Arms"
+            }, {
+                "id": 12,
+                "name": "Back"
+            }, {
+                "id": 14,
+                "name": "Calves"
+            }, {
+                "id": 11,
+                "name": "Chest"
+            }, {
+                "id": 9,
+                "name": "Legs"
+            }, {
+                "id": 13,
+                "name": "Shoulders"
+            }];
 
-        }
+            var equipmentNames = [{
+                "id": 1,
+                "name": "Barbell"
+            }, {
+                "id": 8,
+                "name": "Bench"
+            }, {
+                "id": 3,
+                "name": "Dumbbell"
+            }, {
+                "id": 4,
+                "name": "Gym mat"
+            }, {
+                "id": 9,
+                "name": "Incline bench"
+            }, {
+                "id": 10,
+                "name": "Kettlebell"
+            }, {
+                "id": 7,
+                "name": "none (bodyweight exercise)"
+            }, {
+                "id": 6,
+                "name": "Pull-up bar"
+            }, {
+                "id": 5,
+                "name": "Swiss Ball"
+            }, {
+                "id": 2,
+                "name": "SZ-Bar"
+            }];
+            for (var i = 0; i < data.results.length; i++) {
 
-        $('body').append(
-            "Name:   " + name + '<hr>' + "Directions:   " +
-            description + '<hr>' + "Muscle Category:   " +
-            muscleCategory + '<hr>' + "Equipment Needed:    " +
-            equipment + '<hr><br><br>');
-    }
-    //				if (muscleCategory.equals) {
-    //
-    //				} else {
-    //
-}
+                var name = data.results[i].name;
+
+                var description = data.results[i].description;
+                var muscleCategory = data.results[i].category;
+                var equipment = data.results[i].equipment;
+
+                $.each(muscleCategories, function(key, val) {
+
+                    if (val.id == muscleCategory) {
+                        muscleCategoryText = val.name;
+                        return true;
+                    }
+
+                });
+
+                $.each(equipmentNames, function(key, val) {
+
+                    if (val.id == equipment) {
+                        equipmentText = val.name;
+                        return true;
+                    }
+
+                });
+
+
+                $('#tableData').append("<tr>");
+                $('#tableData').append("<td>" + name + "</td>");
+                $('#tableData').append("<td>" + description + "</td>");
+                $('#tableData').append("<td>" + muscleCategoryText + "</td>");
+                $('#tableData').append("<td>" + equipmentText + "</td>");
+                $('#tableData').append("</tr>");
+
+            }
+
+        });
+
+
+
+
+});
